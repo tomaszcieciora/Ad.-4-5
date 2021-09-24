a8e2b5254017bc6747f350fe813af534d00a1433
Tomasz Ciecióra <tomek.cieciora@gmail.com>
2021-09-24 21:39:29 +0200

    Add project files.

---
+++ Ad. 4, 5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Ad._4__5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Ad. 4
+            int a;
+            string b;
+            double c;
+            a = 10;
+            b= "Szkoła Dotneta";
+            c= 12.5;
+            Console.WriteLine("Ad. 4\n" + a + "\n" + b + "\n" + c);
+
+            //Ad. 5
+
+            Console.WriteLine("\n Ad. 5 \n Hello! \n Give your name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Give your lastname:");
+            string lastName = Console.ReadLine();
+            Console.WriteLine("phone number:");
+            int phone = int.Parse(Console.ReadLine());
+            Console.WriteLine("email:");
+            string email = Console.ReadLine();
+            Console.WriteLine("Weight:");
+            double weight = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("This is your date:" + "\n" + name + "\n" + lastName + "\n" + phone + "\n" + weight);
+        }
+    }
+}
